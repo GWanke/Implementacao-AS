814d7cf1bf80849e6de5fb55afe953997b39853f
Gwanke <ra91671@uem.br>
2020-11-05 11:11:25 -0300

    View home paciente criada. Da pra ser uma home apenas, tem que falar com a equipe.

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const express = require("express")
+const express = require("express");
+const bodyParser = require('body-parser');
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000,function(){
 	console.log ("servidor na porta 3000")
 })
+
+
+app.set('view engine' , 'ejs');
+
+
+app.get('/', (req, res) => {
+  res.render('home.ejs')
+})
+
