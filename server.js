2a2cbe3989e3ba2b4053d7ec6efb1d66b43ee2e7
Gwanke <ra91671@uem.br>
2020-11-10 13:35:24 -0300

    Tela login/Tela home

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-//import FormsUser from "./usuario";
-//import FormsAdm from "./adm";
+
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express()
@@ -10,29 +9,72 @@ app.listen(3000,function(){
 	console.log ("servidor na porta 3000")
 })
 
-// const ControleAcesso = {
-// 	Usuario: {
-// 		render(usuario){
-// 			return <FormsAdm{...usuario}>
-// 		}
-// 	}
-// }
 
+// Strategy Pattern para selecionar a prioridade do user para a exibicao de diferentes paginas.
+
+
+var ControleAcesso = function(){
+	this.prioridade = '';
+};
+
+
+ControleAcesso.prototype = {
+	setStrategy: function(prioridade){
+		this.prioridade=prioridade;
+	},
+
+	path: function(){
+		return this.prioridade.path;
+	}
+};
+
+var Admin = function(){
+	this.path = function(){
+		return "homeADM.ejs"
+	}
+};
+
+var User = function(){
+	this.path = function(){
+		return "homeUSER.ejs"
+	}
+}
 
 
 
 app.set('view engine' , 'ejs');
 
-
-
-app.get('/', (req, res) => {
-	res.render('home.ejs')
+app.get('/login', (req,res) => {
+ 	res.render('login.ejs');
 })
+
+
+app.route('/')
+	.get((req, res) => {
+		res.redirect('login')
+	})
+	.post((req,res) => {
+		var login = req.body.uname;
+			senha = req.body.psw;
+			controleAcesso = new ControleAcesso();
+			admin = new Admin();
+			user = new User();
+		if (login == "login" && senha == "senha"){
+			controleAcesso.setStrategy(admin);
+		}
+		if (login == "login2" && senha == "senha2"){
+			controleAcesso.setStrategy(user)
+		}
+		res.render(controleAcesso.prioridade.path())
+})
+
 
 app.route('/CadastroP')       
 	.get((req,res)=>{
  		res.render('CadastroP.ejs');    //nao precisa de controle de acesso.
  	});
+
+
 app.route('/confCad')
 	.post((req, res,next) => {
   		res.render('confCad.ejs');
@@ -45,5 +87,3 @@ app.route('/confCad')
   		var entrada = { Nome : nome, CPF : cpf ,RG : rg, Email : email ,Nascimento : data, convenio : convenio ,};
   		console.log("Entrada no DB,quando implementado: ",entrada);
 	});
-
-
