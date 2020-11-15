c44b9af0e8a5159a89d30a39a6178d80ab2c73fd
Felipe Watanabe <56563991+Felipe-Honda@users.noreply.github.com>
2020-11-15 20:53:21 -0300

    cadastro modificado
    deletar feito, listagem feita, relatórios mensais feito

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-import express from 'express'
-import * as path from 'path'
-
+const bodyParser = require('body-parser');
+const express = require('express');
 const app = express();
 const port = 3000;
 const router = express.Router();
-const pathr = path.resolve();
 
+app.use(bodyParser.urlencoded({extended : true}))
 
-app.get('/',function(req,res){
-    res.sendFile('CadastroF.html', {root: pathr})
-})
+app.listen(port,function(){
+    console.log("servidor na porta 3000")
+});
 
-app.listen(port,() => console.log("porta " + port))
+app.set('view engine', 'ejs');
+
+app.route('/')
+    .get((req,res) => {
+        res.render('DeletarF.ejs');
+    });
+
+app.get('/editF', (req,res) => {
+    res.render('editF.ejs')
+});
+
+app.get('/listF',(req,res) => {
+    res.render('listF.ejs')
+});
+
+app.get('/relatoriosF',(req,res)=>{
+    res.render('relatorios.ejs')
+});
+
+app.route('/CadConf')
+    .post((req, res,next) => {
+          var nome = req.body.fnome;
+          var cpf = req.body.fcpf
+          var rg = req.body.frg
+          var salario = req.body.fsalario
+          var cargo = req.body.cargo
+          var entrada = { Nome : fnome, CPF : fcpf ,RG : frg, Salario: fsalario , Cargo: fcargo};
+          console.log("Entrada no DB,quando implementado: ",entrada);
+    });
